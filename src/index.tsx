579925696a5d82321b1a2762f21094906136d7dc
Aliaksandr <dfdf1534@gmail.com>
2022-01-24 02:35:19 +0300

    feat: add webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,7 @@
+import ReactDOM from 'react-dom';
+import App from './app';
 
-const root = document.createElement('div')
+const root = document.createElement('div');
+document.body.append(root);
+
+ReactDOM.render(<App>Hello my app</App>, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
-const path = require('path')
-const HtmlWebpackPlugin = ('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const EslintPlugin = require('eslint-webpack-plugin');
+// const eslintOptions = require('.eslintrc.json')
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.tsx'),
     devtool: 'inline-source-map',
-    modules: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node-modules/
-        }]
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        port: 8080,
+        open: false,
+        static: './dist'
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: [".tsx", ".ts", ".js", "jsx"]
+    },
+    module: {
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(?:ico|png|jpe?g|gif|svg)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+    ]
     },
     output: {
         filename: 'bundle.js',
@@ -23,6 +46,10 @@ module.exports = {
             {
                 template: path.join(__dirname, 'index.html' )
             }
-        )
+        ),
+        // new CopyWebpackPlugin({
+        //     patterns: [{from: path.resolve(__dirname, './src/assets'), to: 'assets'}]
+        // }),
+        // new EslintPlugin(eslintOptions)
     ]
 }
